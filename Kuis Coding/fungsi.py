4c03aefd26409d008b747955724bf1e0b3b04d54
putrisft <putrisarahf6@gmail.com>
2023-09-16 12:42:15 +0700

    first commit

---
+++ Kuis Coding/fungsi.py
@@ -0,0 +1,17 @@
+"""
+TODO:
+Buatlah sebuah fungsi bernama "minimal" dengan ketentuan berikut.
+- Menerima dua buah argumen berupa number, yaitu a dan b.
+- Mengembalikan nilai terkecil antara a atau b.
+- Bila nilai keduanya sama, kembalikan dengan nilai a.
+"""
+
+# TODO: Silakan buat kode Anda di bawah ini.
+def minimal(a,b):
+    if a > b:
+        return b
+    else:
+        return a
+print(minimal(10,8))
+  
+  
